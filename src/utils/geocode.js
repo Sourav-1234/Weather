e755ace9c03c9c267c4c89d3375f49a4cff52fb7
Sourav Saha <sourav024saha@gmail.com>
2023-08-26 16:56:20 +0530

    Update geocode.js
    Removing the access key from the file

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoic291cmF2LTI0IiwiYSI6ImNrdGxuYXc5cjA3dWQydWwyOTRxY295eDAifQ.YFGoyMR5XUwpjjfnaePExQ'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?proximity=-74.70850,40.78375&access_token=#'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
